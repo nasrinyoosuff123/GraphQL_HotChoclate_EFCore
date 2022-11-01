c8786e1462a6bd1f6f14da5b10e6b19becc270ef
Nasrin Yoosuff <104313047+nasrinyoosuff123@users.noreply.github.com>
2022-10-31 23:08:11 -0700

    Add files via upload

---
+++ GraphQL_HotChoclate_EFCore/Migrations/20210426110349_Initial.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GraphQL_HotChoclate_EFCore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Designation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+            
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Designation", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Full Stack Developer", "Nasrin Yoosuff" },
+                    { 2, "SSE", "DQ" },
+                    { 3, "Software Engineer", "Eliza" },
+                    { 4, "Database Developer", "Sheriff" },
+                    { 5, "Cloud Engineer", "Reddy" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ GraphQL_HotChoclate_EFCore/Models/DatabaseContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL_HotChoclate_EFCore.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        #region Seed Data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id =1,
+                    Name = "Nasrin Yoosuff",
+                    Designation = "Full Stack Developer"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "DQ",
+                    Designation = "SSE"
+                },
+                new Employee 
+                {
+                    Id = 3,
+                    Name = "Eliza",
+                    Designation = "Software Engineer"
+                },
+                new Employee
+                {
+                    Id = 4,
+                    Name = "Sheriff",
+                    Designation = "Database Developer"
+                },
+                new Employee
+                {
+                    Id = 5,
+                    Name = "Reddy",
+                    Designation = "Cloud Engineer"
+                }
+                );
+        }
+        #endregion
+
+    }
+}
